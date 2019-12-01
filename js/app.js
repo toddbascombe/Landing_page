5bee1b5880e3d6d9d5077a65aa09b04b5a3aed6a
tech@ccnyalumni.org <toddbascombe@gmail.com>
2019-12-01 16:19:16 -0500

    add active state to each section

--- js/app.js
+++ js/app.js
@@ -53,6 +53,26 @@ const attributesAutomation = (elementTag, element, text, classInfo) => {
   return element;
 };
 
+const Page_yCoord_IntervalSection = menuLink_index => {
+  const interval_between_sections = 800;
+  const yCoord_page_libary = [400];
+  for (let i = 1; i < data_nav.length; i++) {
+    let yCoordAdd = yCoord_page_libary[i - 1] + interval_between_sections;
+    yCoord_page_libary.push(yCoordAdd);
+  }
+  return yCoord_page_libary[menuLink_index];
+};
+
+const add_class = element_target => {
+  const section = document.querySelector(element_target.hash);
+  section.classList.add("your-active-class");
+};
+
+const remove_class = () => {
+  for (section of data_nav) {
+    section.classList.remove("your-active-class");
+  }
+};
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -71,7 +91,21 @@ const navElementsAdder = () => {
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
-
+const scoller = event => {
+  const menu_links = document.querySelectorAll(".menu__link");
+  for (let indexOfLink = 0; indexOfLink < menu_links.length; indexOfLink++) {
+    if (
+      event.target.attributes.href.value ==
+      menu_links[indexOfLink].attributes.href.value
+    ) {
+      event.preventDefault();
+      const ycoord = Page_yCoord_IntervalSection(indexOfLink);
+      window.scrollTo({ top: ycoord, behavior: "smooth" });
+      remove_class();
+      add_class(event.target);
+    }
+  }
+};
 /**
  * End Main Functions
  * Begin Events
@@ -81,8 +115,8 @@ const navElementsAdder = () => {
 // Build menu
 window.addEventListener("DOMContentLoaded", navElementsAdder);
 
+// Scroll to section on link click
+document.addEventListener("click", scoller);
 let end = performance.now();
 console.log("performance Metric of code " + (end - start));
-// Scroll to section on link click
-
 // Set sections as active
